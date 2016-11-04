d2784f1c8aee4b7997490fd872bebe60a8232487
julianromero83 <rom16015@byui.edu>
2016-11-04 15:49:31 -0700

    Created scriptureApp github repo
    julian romero

---
+++ RandomScripture/app/src/main/java/matthewmcguff/randomscripture/RandomScriptureActivity.java
@@ -0,0 +1,47 @@
+package matthewmcguff.randomscripture;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class RandomScriptureActivity extends AppCompatActivity {
+    private ScriptureRef mScriptureRef = new ScriptureRef();
+    private ColorWheel mColorWheel = new ColorWheel();
+    // Declare our view variables
+    private TextView mScriptureTextView;
+    private Button mShowScriptureButton;
+    private RelativeLayout mRelativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random_scripture);
+
+        // Assign Views to create member variables.
+        mScriptureTextView = (TextView) findViewById(R.id.scriptureTextView);
+        mShowScriptureButton = (Button) findViewById(R.id.showScriptureButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String scripture = mScriptureRef.getScripture();
+                int color = mColorWheel.getColor();
+
+                //update the screen with our dynamic scripture.
+                mScriptureTextView.setText(scripture);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowScriptureButton.setTextColor(color);
+            }
+        };
+        mShowScriptureButton.setOnClickListener(listener);
+    }
+}
+//Comment test for github -Julian Romero
---
+++ RandomScripture/app/src/main/java/matthewmcguff/randomscripture/ScriptureRef.java
@@ -0,0 +1,31 @@
+package matthewmcguff.randomscripture;
+
+import java.util.Random;
+
+public class ScriptureRef {
+    //fields (member variables)
+    private String[] mScriptures = {
+            "If ye love me, Keep my Commandments.",
+            "And my Father dwelt in a Tent.",
+            "Jesus Wept.",
+            "...Wickedness never was happiness",
+            "...Ye shall know the truth and the truth shall set you free",
+            "This is my work and my glory to bring to pass the immortality and eternal life of man",
+            "By the power of the holy ghost ye may know the truth of all things",
+            "In the beginning God created the heavens and the earth",
+            "...When thou are converted, strengthen thy brethren",
+            "Thy will be done." };
+
+    //Methods - actions the object can take
+    public String getScripture(){
+
+        String scripture;
+
+        //randomly select a scripture.
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mScriptures.length);
+        scripture = mScriptures[randomNumber];
+
+        return scripture;
+    }
+}
