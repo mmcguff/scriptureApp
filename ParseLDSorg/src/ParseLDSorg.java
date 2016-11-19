0e24af1488be4ff03f6f0c6473ae3f25e432ec6e
julianromero83 <rom16015@byui.edu>
2016-11-18 22:06:20 -0800

    LDSorg Parse working
    ParseLDSorg.java can now parse through LDS.org scripture html and return
    scripture verse content.

--- ParseLDSorg/src/ParseLDSorg.java
+++ ParseLDSorg/src/ParseLDSorg.java
@@ -3,40 +3,36 @@ import java.io.IOException;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Node;
 
 
 
 /*
  * Get jsoup-1.10.1.jar from jsoup.org/download
  * Pull into 'Libraries' from downloaded location
- */
-
-/**
- *
  * @author Julian
  */
 public class ParseLDSorg {
     public static void main (String[] args) throws IOException{
-        String url = "https://www.lds.org/scriptures/bofm/1-ne/1?lang=eng";
+        
+        String url = "https://www.lds.org/scriptures/bofm/1-ne/6?lang=eng";
         Document doc = Jsoup.connect(url).get();
         String verse;
         
-        Elements scripture = doc.getElementsByTag("p");
-        //Node content = doc.childNode(1);
-        String book = (scripture.toString());
+        Elements scripture = doc.select("p[class]").removeAttr("uri");
+        scripture.select("a[class=bookmark-anchor dontHighlight]").remove();
+        scripture.select("a").unwrap();
+        scripture.select("span").remove();
+        scripture.select("sup").remove();
+        String book = scripture.toString();
         String [] verses = book.split("\n");
-        for(int i = 0; i < verses.length; i++)
-        {
+        for (int i = 0; i < verses.length; i++) {
             verse = verses[i];
-            verse = verse.substring(verse.indexOf("</span>")+7);
-            verse = verse.replaceAll("<sup class=\"studyNoteMarker\">a</sup>", "");
+            verse = verse.substring((verse.indexOf("<p class=\"\"")+12),verse.indexOf("</p>"));
             verses[i] = verse;
         }
-        for(int j = 1; j < verses.length - 3; j++)
+        for(int j = 0; j < verses.length; j++)
         {
-            System.out.println(j + " " + verses[j]);
+            System.out.println(j+1 + " " + verses[j]);
         }
-        //System.out.println(content.toString());
     }
 }
